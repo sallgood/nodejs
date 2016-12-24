7ba5f2f92ab82c0cca86fb4d7b92bae5f82c2f11
sallgood <zameyer@gmail.com>
2016-12-23 20:10:35 -0600

    Added a Sonarr history intent.

--- pegleg.js
+++ pegleg.js
@@ -15,6 +15,7 @@ exports.handler = function(event, res, callback) {
             port: 8081
     });
     
+    //Queries CouchPotato for $moviename and returns speech/text asking if user wants to download the top hit
     if(event.body.result.metadata.intentName == "search") {
         console.log(event.body.result.parameters.action + " for " + event.body.result.parameters.moviename);
         couchpotato.get("search", {q: event.body.result.parameters.moviename}).then(function (result) {
@@ -39,6 +40,7 @@ exports.handler = function(event, res, callback) {
         }).catch(function (err) {
           throw new Error("There was a error processing the request: " + err);
         });
+    //If user says "yes" to download question above, this takes the imdb.id (tt1234567) and adds it to the CP queue
     } else if (event.body.result.metadata.intentName == "download_movie - yes") {
         var fcontext = event.body.result.contexts.filter(function(d) { return d.name == 'movie'; })[0];
 
@@ -57,22 +59,23 @@ exports.handler = function(event, res, callback) {
             
             callback(res, JSON.stringify(ret))
         });
-    } else if (event.body.result.metadata.intentName == "shows") {
+    //returns next episode on Sonarr's calendar and asks user if they want to hear the tagline
+    } else if (event.body.result.metadata.intentName == "shows - calendar") {
         var today = new Date();
         var yesterday = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1);
-        var tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 6);
+        var tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
         sonarr.get("calendar", { "start": yesterday.toISOString(), "end": tomorrow.toISOString() }).then(function (result) {
             show = result[0];
             console.log(show);
             if(show == undefined) {
                 var ret = {
-                    speech: "You have no shows airing in the next week.",
-                    displayText: "You have no shows airing in the next week."
+                    speech: "You have no shows airing in the next month.",
+                    displayText: "You have no shows airing in the next month."
                 };
             } else {
                 var showTime = new Date(show.airDateUtc); //airDateUtc: '2017-01-01T20:30:00Z'
-                stdate = showTime.toString().substring(0, showTime.toString().length-29); //shorten our UTC date down to something sensible
-                srtime = showTime.toString().substring(16, showTime.toString().length-18); //just snag the converted time
+                var stdate = showTime.toString().substring(0, showTime.toString().length-29); //shorten our UTC date down to something sensible
+                var srtime = showTime.toString().substring(16, showTime.toString().length-18); //just snag the converted time
                 var ret = {
                     speech: show.series.title + " will air on " + show.series.network + " on "  + stdate + " at " + srtime + ".  Would you like to hear the overview?",
                     displayText: show.series.title + " will air on " + show.series.network + " on "  + stdate + " at " + srtime + ".  Would you like to hear the overview?",
@@ -91,6 +94,7 @@ exports.handler = function(event, res, callback) {
         }, function (err) {
             throw new Error("There was a error processing the request: " + err);
         });
+    // if the user said "yes" to the tagline questions we play the tagline
     } else if (event.body.result.metadata.intentName == "show_overview - yes") {
         var scontext = event.body.result.contexts.filter(function(d) { return d.name == 'show'; })[0];
         var ret = {
@@ -98,5 +102,44 @@ exports.handler = function(event, res, callback) {
             displayText: scontext.parameters.overview            
         };
         callback(res, JSON.stringify(ret));
+    } else if (event.body.result.metadata.intentName == "shows - history") {
+        sonarr.get("history", { "page": 1, "pageSize": 30, "sortkey": "date", "sortDir": "desc" }).then(function (result) {
+            //console.log(result);
+            var i=0;
+            var j=0;
+            var mySpeech = "";
+            // We only want to list things that completed successfully.
+            // It seems the results with eventType as "downloadFolderImported" were downloaded and moved to their final folders successfully.
+            for(i; i < 30 && j < 5; i++) {
+                if (result.records[i].eventType == "downloadFolderImported") {
+                    mySpeech = mySpeech + result.records[i].series.title + ", Season " + result.records[i].episode.seasonNumber + " Episode " + result.records[i].episode.episodeNumber + 
+                        ", " + result.records[i].episode.title + ". ";
+                    j++;
+                }
+            }
+            if(result == undefined) {
+                var ret = {
+                    speech: "You have no recent downloads looking back two months.",
+                    displayText: "You have no recent downloads looking back two months."
+                };
+            } else {
+                var ret = {
+                    speech: "Here are the last five downloads. " + mySpeech,
+                    displayText: "Here are the last five downloads. " + mySpeech,
+                    /* contextOut: [
+                      {
+                        name: "show",
+                        parameters: {
+                            overview: show.overview
+                        }
+                      } 
+                    ] */
+                }; 
+            }            
+            callback(res, JSON.stringify(ret));
+        }, function (err) {
+            throw new Error("There was a error processing the request: " + err);
+        });
+    // if the user said "yes" to the tagline questions we play the tagline
     }
 };
